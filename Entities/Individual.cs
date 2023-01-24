8fdd527b1b03f2c89cb5b4e8b048e876a91164d5
Amarante Silva <amaranthwfs@gmail.com>
2023-01-24 15:20:06 -0300

    ex do curso C#

---
+++ Entities/Individual.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Exercicio.Entities
+{
+    internal class Individual:TaxPayer
+    {
+        public double HearthExpenditures { get; set; }
+
+        public Individual() 
+        {
+        }
+
+        public Individual(string name, double anualIncome , double hearthExpenditures):base(name,anualIncome)
+        {
+            HearthExpenditures = hearthExpenditures;
+        }
+
+        public override double Tax()
+        {
+            if (AnualIncome < 20000.0)
+            {
+                return AnualIncome * 0.15 - HearthExpenditures * 0.5;
+            }
+            else 
+            {
+               return AnualIncome * 0.25 - HearthExpenditures * 0.5;
+            }
+
+            
+        }
+    }
+}
